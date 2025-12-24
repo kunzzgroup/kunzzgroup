a12de4bf96b68624c9d8814ae4d5f49d07bdaaa0
darrell <darrellchua118@gmail.com>
2025-12-24 09:21:41 +0800

    D207

--- backend/qna.php
+++ backend/qna.php
@@ -894,7 +894,7 @@ require_once 'session_check.php';
                     6: height - 380,
                     7: height - 420,
                     8: height - 650,
-                    9: height - 725,
+                    9: height - 727,
                     10: height - 805,
                 };
 
