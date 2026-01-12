7fb26ef59fd74557edcf08072e74f7e3048338c6
darrell <darrellchua118@gmail.com>
2026-01-12 14:26:53 +0800

    D500

--- backend/sidebar.php
+++ backend/sidebar.php
@@ -1267,6 +1267,11 @@ body.sidebar-transition {
                         </div>
                     </div>
                 </div>
+                <div class="menu-item-wrapper">
+                    <a href="corporate_blueprint_edit.php" class="informationmenu-item">
+                        企业蓝图管理
+                    </a>
+                </div>
             </div>
         </div>
         <?php endif; ?>
