5de1b3363e777f63013ab84478288b0bc9d56b47
ZheJin0527 <zjwu0407@gmail.com>
2026-01-16 10:44:25 +0800

    6

--- dishware_stock.php
+++ dishware_stock.php
@@ -17,7 +17,7 @@ header('Expires: 0');
         * {
             margin: 0;
             padding: 0;
-            box-sizing: border-box;hh
+            box-sizing: border-box;
         }
         
         body {
