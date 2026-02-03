021d58956b563ee777dae6986ff49542ce3f3202
ZheJin0527 <zjwu0407@gmail.com>
2026-02-03 17:12:46 +0800

    493

--- backend/generatecode.php
+++ backend/generatecode.php
@@ -9,7 +9,7 @@ require_once 'session_check.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <title>生成申请码管理系统</title>
+    <title>职员管理系统</title>
     <style>
         * {
             margin: 0;
