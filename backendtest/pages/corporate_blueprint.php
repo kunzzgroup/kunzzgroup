cff2e657c93d74eb970febe3327610af5f174328
kunzzit01 <kunzzit01@gmail.com>
2026-02-06 15:31:39 +0800

    18

--- backendtest/pages/corporate_blueprint.php
+++ backendtest/pages/corporate_blueprint.php
@@ -2,21 +2,6 @@
 session_start();
 ob_start();
 
-// 设置字符编码
-header('Content-Type: text/html; charset=UTF-8');
-
-// 加载JSON数据 - 文件在backend目录中
-$jsonFile = __DIR__ . '../backend/corporate_strategy.json';
-$strategyData = null;
-
-if (file_exists($jsonFile)) {
-    $jsonContent = file_get_contents($jsonFile);
-    $strategyData = json_decode($jsonContent, true);
-    
-    if (json_last_error() !== JSON_ERROR_NONE) {
-        $strategyData = null;
-    }
-}
 ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
