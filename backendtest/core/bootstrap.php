c3aef4614b7b70d7e6d21dad1507600e1180d931
kunzzit01 <kunzzit01@gmail.com>
2026-02-10 11:29:26 +0800

    47

--- backendtest/core/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-require_once __DIR__ . '/check_permissions.php';
-require_once __DIR__ . '/session_check.php';
-require_once __DIR__ . '/sidebar.php';
-require_once __DIR__ . '/check_restaurant_setup.php';
-?>
