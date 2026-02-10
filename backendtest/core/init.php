8b2e989fc8a32deb9a3c353124dd3482431a94c3
kunzzit01 <kunzzit01@gmail.com>
2026-02-10 16:12:38 +0800

    77

--- backendtest/core/init.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Absolute project root
-define('ROOT_PATH', dirname(__DIR__));
-define('CORE_PATH', ROOT_PATH . '/core');
-define('API_PATH', ROOT_PATH . '/api');
-define('MODULE_PATH', ROOT_PATH . '/modules');
-define('CMS_PATH', ROOT_PATH . '/cms');
-?>
--- backendtest/stock/stocklistall.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once dirname(__DIR__) . '/core/init.php';
-require_once CORE_PATH . '/bootstrap.php';
-?>
