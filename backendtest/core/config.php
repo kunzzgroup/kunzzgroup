779a890dd22c23ddc1290b4094cdca4b144c86a7
kunzzit01 <kunzzit01@gmail.com>
2026-02-10 13:01:39 +0800

    58

--- backendtest/core/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'u690174784_kunzz');
-define('DB_USER', 'u690174784_kunzz');
-define('DB_PASS', 'Kunzz1688');
-define('DB_CHARSET', 'utf8mb4');
