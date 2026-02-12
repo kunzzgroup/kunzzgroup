b3efdb1a247de9fa20d549fda2a05f0ff6191d65
kunzzit01 <kunzzit01@gmail.com>
2026-02-12 11:13:07 +0800

    180

--- backendtest/modules/generatecode.php
+++ backendtest/modules/generatecode.php
@@ -10,7 +10,7 @@ require_once CORE_PATH . '/session_check.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/generatecode.css">
+    <link rel="stylesheet" href="modules/css/generatecode.css">
     <title>生成申请码管理系统</title>
 
 </head>
