21ae5e8b5dff0c9e6e7a7efc905b8162f0507888
kunzzit01 <kunzzit01@gmail.com>
2026-02-13 12:57:03 +0800

    240

--- backend/generatecode.php
+++ backend/generatecode.php
@@ -9,8 +9,9 @@ require_once 'session_check.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <title>职员管理系统</title>
     <link rel="stylesheet" href="css/generatecode.css">
+    <title>职员管理系统</title>
+    
 </head>
 <body>
     <?php include 'sidebar.php'; ?>
