a59b1ce789d87528376ff4dbea27df6817602bd1
kunzzit01 <kunzzit01@gmail.com>
2026-02-13 16:22:56 +0800

    262

--- backend/stockeditall.php
+++ backend/stockeditall.php
@@ -13,7 +13,7 @@ require_once 'session_check.php';
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="animation.css" />
-    <link rel="stylesheet" href="../css/stockeditall.css" />
+    <link rel="stylesheet" href="/backend/css/stockeditall.css" />
     
 </head>
 <body>
