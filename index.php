976c9d8f1aa9753ad37c66371ba27b22ca4df769
kunzzit01 <kunzzit01@gmail.com>
2026-02-23 10:08:45 +0800

    285

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-// 永久重定向到 frontend/index.php
-header("Location: frontend/index.php");
-exit;
+// Silently include the frontend homepage to keep the URL clean
+require 'frontend/index.php';
 ?>
