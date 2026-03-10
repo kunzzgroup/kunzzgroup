a2c6cfeb4b9d1ab749260dfe51a471f1d8ee001e
kunzzit01 <kunzzit01@gmail.com>
2026-03-10 17:02:36 +0800

    605

--- backend/menu_api.php
+++ backend/menu_api.php
@@ -8,10 +8,12 @@
  * 2. 增加字段: 在 renderItemHTML() 中增加 <div> 或 <input>，并在 actionAdd/Edit 中添加该字段。
  */
 
-header('Content-Type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
+if (isset($_REQUEST['action'])) {
+    header('Content-Type: application/json; charset=utf-8');
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type');
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
